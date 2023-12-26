f18f6896788b0b0c9e39021631e849c95627114a
bumjun2 <jbj3713@naver.com>
2023-12-26 11:39:29 +0900

    # 30. 쿠키 세션 개념 파악하기

--- src/main/java/com/spring/mvc/chap05/service/MemberService.java
+++ src/main/java/com/spring/mvc/chap05/service/MemberService.java
@@ -3,12 +3,15 @@ package com.spring.mvc.chap05.service;
 
 import com.spring.mvc.chap05.dto.request.LoginRequestDTO;
 import com.spring.mvc.chap05.dto.request.SinUpRequestDTO;
+import com.spring.mvc.chap05.dto.response.LoginUserResponseDTO;
 import com.spring.mvc.chap05.entity.Member;
 import com.spring.mvc.chap05.repository.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 @Service
 @RequiredArgsConstructor
@@ -24,10 +27,11 @@ public class MemberService {
         // 패스워드 인코딩하여 엔터티로 변환해서 전달
         return mapper.save(dto.toEntity(encoder));
     }
+
     // 로그인 검증 처리
     public LoginResults authenticate(LoginRequestDTO dto) {
 
-        Member foundMember = mapper.findMember(dto.getAccount());
+        Member foundMember = getMember(dto.getAccount());
 
         if (foundMember == null){ // 회원가입 안한 상태
             log.info("{} - 회원가입이 필요합니다.", dto.getAccount());
@@ -44,6 +48,37 @@ public class MemberService {
 
         log.info("{}님 로그인 성공!", foundMember.getName());
         return LoginResults.SUCCESS;
+    }
+
+    private Member getMember(String account) {
+        return mapper.findMember(account);
+    }
+
+    // 아이디 중복 검사 체크
+    public boolean checkDuplicateValue(String type, String keyword){
+        return mapper.isDuplicate(type, keyword);
+    }
+
+    // 세션을 사용해서 일반 로그인 유지
+    public void maintainLoginState(HttpSession session, String account){
+        // 세션은 서버에서만 유일하게 보관되는 데이터로서
+        // 로그인 유지등 상태유지가 필요할 때 사용되는 개념입니다.
+        // 세션은 쿠키와 달리 모든 데이터를 저장할 수 있습니다.
+        // 세션의 수명은 설정한 수명시간에 영향을 받고 브라우저의 수명과 함께한다.
+
+        // 현재 로그인한 사람의 모든 정보 조회
+        Member member = getMember(account);
+        LoginUserResponseDTO dto = LoginUserResponseDTO.builder()
+                .account(member.getAccount())
+                .email(member.getEmail())
+                .nickName(member.getName())
+                .build();
+
+        // 세션에 로그인한 회원의 정보 저장
+        // 로그인한 사람의 정보를 누가 기억하냐 서버가 기억한다 ? 세션 클라이언트가 저장한다 ? 쿠키
+        session.setAttribute("login", dto);
+        // 세션도 수명을 설정해야 함.
+        session.setMaxInactiveInterval(60 * 60); // 1시간이 지나면 로그인이 풀림
     }
 
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -5,8 +5,49 @@
 <head>
     <meta charset="UTF-8">
     <title>Web study</title>
+    <%@ include file="include/static-head.jsp"%>
+    <style>
+        h1{
+            margin: 200px auto;
+            width: 40%;
+            font-size: 40px;
+            font-weight: 700;
+            color: orange;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    <h1>안녕하세요~~~~~</h1>
+
+    <%
+
+        String userName = "방문자";
+        //클라이언트에게 쿠키를 검사
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("login")){
+                userName = cookie.getValue();
+            }
+        }
+    %>
+
+    <%@ include file="include/header.jsp"%>
+
+<%--    <h1><%= userName %> 안녕하세요~~~~~</h1>--%>
+
+<%--    서버에서 model이나 redirectAttributes에 담아 놓은 데이터는
+        ${requestScope.aaa} 로 참조 가능하고 requestScrope는 생략 가능
+
+        그런데 세션에 담은 데이터는
+        ${sessionScope.aaaa}로 참조가능하고 model에 같은 이름의 데이터가 있다면
+        sessionScope를 생략가능하다.--%>
+    <c:if test="${login == null}">
+        <h1>방문자님 안눙하세요 !!!!</h1>
+    </c:if>
+
+    <c:if test="${login != null}">
+        <h1>${login.nickName}님 하리룰</h1>
+    </c:if>
+
 </body>
 </html>
