74523f45c60efa76c9d6b41e7174a841d0a54f67
bumjun2 <jbj3713@naver.com>
2023-12-27 09:39:41 +0900

    # 인터셉터 기초

---
+++ src/main/java/com/spring/mvc/interceptor/AfterLoginInterceptor.java
@@ -0,0 +1,35 @@
+package com.spring.mvc.interceptor;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Configuration
+@Slf4j
+public class AfterLoginInterceptor implements HandlerInterceptor {
+
+    // 로그인한 이후 비회원만 접근할 수 있는 페이지 접근 차단
+
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler) throws Exception {
+
+        log.info("after login interceptor execute!!");
+
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("login") != null){
+            response.sendRedirect("/"); // 라다이렉트처리 홈으로 이동
+            return false; // 나가 꺼져 ~
+        }
+
+        return true; // 들어왕 자기양
+    }
+}
